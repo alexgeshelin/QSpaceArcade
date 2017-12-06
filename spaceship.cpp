b06e98242adfe071b79c4af2b394aad2e6960673
Olexandr Heshelin <ohesheli@e1r6p16.unit.ua>
2017-12-06 19:24:04 +0200

    added WASD controls

--- spaceship.cpp
+++ spaceship.cpp
@@ -1,6 +1,7 @@
 #include "spaceship.h"
 
-Spaceship::Spaceship() : QGraphicsPixmapItem(QPixmap(QString(":/resources/spaceship.png")))
+Spaceship::Spaceship() :
+    QGraphicsPixmapItem(QPixmap(QString(":/resources/spaceship.png")))
 {
     setShapeMode(QGraphicsPixmapItem::MaskShape);
     setScale(0.5);
@@ -15,66 +16,69 @@ Spaceship::Spaceship() : QGraphicsPixmapItem(QPixmap(QString(":/resources/spaces
 }
 
 void Spaceship::keyPressEvent(QKeyEvent *event) {
-    if (event->key() == Qt::Key_Up)
+    if (event->key() == Qt::Key_Up || event->key() == Qt::Key_W)
         directionUp = true;
-    if (event->key() == Qt::Key_Down)
+    if (event->key() == Qt::Key_Down || event->key() == Qt::Key_S)
         directionDown = true;
-    if (event->key() == Qt::Key_Right)
+    if (event->key() == Qt::Key_Right || event->key() == Qt::Key_D)
         directionRight = true;
-    if (event->key() == Qt::Key_Left)
+    if (event->key() == Qt::Key_Left || event->key() == Qt::Key_A)
         directionLeft = true;
 }
 
 void Spaceship::keyReleaseEvent(QKeyEvent *event) {
-    if (event->key() == Qt::Key_Up)
+    if (event->key() == Qt::Key_Up || event->key() == Qt::Key_W)
         directionUp = false;
-    if (event->key() == Qt::Key_Down)
+    if (event->key() == Qt::Key_Down || event->key() == Qt::Key_S)
         directionDown = false;
-    if (event->key() == Qt::Key_Right)
+    if (event->key() == Qt::Key_Right || event->key() == Qt::Key_D)
         directionRight = false;
-    if (event->key() == Qt::Key_Left)
+    if (event->key() == Qt::Key_Left || event->key() == Qt::Key_A)
         directionLeft = false;
 }
 
 void Spaceship::advance(int phase) {
-    if (directionUp && !directionDown && !(directionRight ^ directionLeft))
-    {
-        y -= 1;
+    if (phase) {
+        if (directionRight && !directionLeft && !(directionDown ^ directionUp))
+        {
+            x += 1;
+        }
+        else if (directionUp && !directionDown && !(directionRight ^ directionLeft))
+        {
+            y -= 1;
+        }
+
+        else if (directionDown && !directionUp && !(directionRight ^ directionLeft))
+        {
+            y += 1;
+        }
+        else if (directionLeft && !directionRight && !(directionDown ^ directionUp))
+        {
+            x -= 1;
+        }
+        else if (directionUp && directionRight && !directionLeft && !directionDown)
+        {
+            y -= 0.7071;
+            x += 0.7071;
+        }
+        else if (directionDown && directionRight && !directionLeft && !directionUp)
+        {
+            y += 0.7071;
+            x += 0.7071;
+        }
+        else if (directionDown && directionLeft && !directionRight && !directionUp)
+        {
+            y += 0.7071;
+            x -= 0.7071;
+        }
+        else if (directionUp && directionLeft && !directionRight && !directionDown)
+        {
+            y -= 0.7071;
+            x -= 0.7071;
+        }
+        checkBounds();
+        setPos(x, y);
     }
-    else if (directionUp && directionRight && !directionLeft && !directionDown)
-    {
-        y -= 0.7071;
-        x += 0.7071;
-    }
-    else if (directionRight && !directionLeft && !(directionDown ^ directionUp))
-    {
-        x += 1;
-    }
-    else if (directionDown && directionRight && !directionLeft && !directionUp)
-    {
-        y += 0.7071;
-        x += 0.7071;
-    }
-    else if (directionDown && !directionUp && !(directionRight ^ directionLeft))
-    {
-        y += 1;
-    }
-    else if (directionDown && directionLeft && !directionRight && !directionUp)
-    {
-        y += 0.7071;
-        x -= 0.7071;
-    }
-    else if (directionLeft && !directionRight && !(directionDown ^ directionUp))
-    {
-        x -= 1;
-    }
-    else if (directionUp && directionLeft && !directionRight && !directionDown)
-    {
-        y -= 0.7071;
-        x -= 0.7071;
-    }
-    checkBounds();
-    setPos(x, y);
 }
 
 void Spaceship::checkBounds() {
